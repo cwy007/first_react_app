aadcaba18f04686601de79d0cd8ff5da3d185677
chenweiyan <chanweiyan007@gmail.com>
2019-12-31 09:51:41 +0800

    tem commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ClickCounter from './ClickCounter';
 import * as serviceWorker from './serviceWorker';
+import ControlPanel from './ControlPanel';
 
-ReactDOM.render(<ClickCounter />, document.getElementById('root'));
+ReactDOM.render(<ControlPanel />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
